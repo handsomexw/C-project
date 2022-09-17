1c4216f3e3d778ec3f1ce026d570e266de653041
xin wang <1804933537@qq.com>
2022-09-17 14:08:51 +0800

    ftp协议客户端

---
+++ ftp_clint/tools.h
@@ -0,0 +1,15 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define ERROR(func) printf("file:%s func:%s line:%d %m\n",__FILE__,func,__LINE__)
+
+//	从键盘获取字符串
+char* get_str(char* str,size_t len);
+//	从键盘输入密码
+char* get_pass(char* pass,size_t len,bool is_show);
+//	文件数据发送
+void file_oi(int ofd,int ifd);
+#endif//TOOLS_H
